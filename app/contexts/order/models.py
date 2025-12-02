ebd305fa3f589c58f96c083e845daaa12af7b53d
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-12-02 21:21:46 +0000

    add order models with migration

---
+++ app/contexts/order/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    JSON,
+    Boolean,
+    func,
+)
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, nullable=False)
+
+    pricing_snapshot = Column(JSON, nullable=False)
+
+    final_amount = Column(Integer, nullable=False)
+
+    is_completed = Column(Boolean, nullable=False, default=False)
+
+    reservation_id = Column(
+        Integer, 
+        ForeignKey("reservations.id"), 
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    reservation = relationship("reservation", back_populates= "orders")
