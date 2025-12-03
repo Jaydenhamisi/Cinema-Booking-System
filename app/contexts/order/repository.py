c103e213ffaa9800a7c6cfbefe26a1157ce3d24e
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-12-03 11:32:13 +0000

    added repository file for OrderContext

---
+++ app/contexts/order/repository.py
@@ -0,0 +1,37 @@
+# app/contexts/order/repository.py
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from .models import Order
+
+
+
+class OrderRepository:
+
+    def get_order_by_id(self, db: Session, order_id: int):
+        return db.get(Order, order_id)
+    
+
+    def list_orders(self, db: Session, completed_only: bool = True):
+        stmt = select(Order)
+        if completed_only:
+            stmt = stmt.where(Order.is_completed == True)
+        return db.scalars(stmt).all()
+    
+    
+    def create(self, db: Session, order: Order):
+        db.add(order)
+        db.commit()
+        db.refresh(order)
+        return order
+    
+    def save(self, db: Session, order: Order):
+        db.add(order)
+        db.commit()
+        db.refresh(order)
+        return order
+
+
+    
+    
+
