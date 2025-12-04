b02989116e3392d89aa49c3bee5c82771bb23ad8
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-12-04 15:55:46 +0000

    added get_order_by_reservation_id to repository file in OrderContext

--- app/contexts/order/repository.py
+++ app/contexts/order/repository.py
@@ -12,8 +12,8 @@ class OrderRepository:
         return db.get(Order, order_id)
     
 
-    def list_orders(self, db: Session, completed_only: bool = True):
-        stmt = select(Order)
+    def list_orders(self, db: Session, user_id:int, completed_only: bool = True):
+        stmt = select(Order).where(Order.user_id == user_id)
         if completed_only:
             stmt = stmt.where(Order.is_completed == True)
         return db.scalars(stmt).all()
@@ -30,8 +30,32 @@ class OrderRepository:
         db.commit()
         db.refresh(order)
         return order
-
+    
+    def get_user_order_by_id(self, db: Session, order_id: int, user_id: int):
+        stmt = select(Order).where(
+            Order.id == order_id,
+            Order.user_id == user_id
+        )
+        return db.scalar(stmt)
 
     
+    def list_user_orders(self, db, user_id, completed_only=True):
+        stmt = (
+            select(Order)
+            .where(Order.user_id == user_id)
+            .order_by(Order.created_at.desc())
+        )
+
+        if completed_only:
+            stmt = stmt.where(Order.is_completed == True)
+
+        return db.scalars(stmt).all()
     
+
+    def get_order_by_reservation_id(self, db, reservation_id):
+        stmt = select(Order).where(Order.reservation_id == reservation_id)
+        
+        return db.scalar(stmt)
+
+
 
