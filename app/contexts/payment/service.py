af5479f1c8c8509906ce4b388f349250cf169050
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-12-12 00:22:44 +0000

    added service.py to PaymentContext

---
+++ app/contexts/payment/service.py
@@ -0,0 +1,117 @@
+from sqlalchemy.orm import Session
+
+from app.core.errors import ValidationError, NotFoundError, ConflictError
+from app.shared.services.event_publisher import publish_event
+
+from .models import PaymentAttempt, PaymentStatus
+from .repository import PaymentRepository
+from .events import (
+    payment_attempt_pending_event,
+    payment_attempt_succeeded_event,
+    payment_attempt_failed_event
+)
+
+repo = PaymentRepository()
+
+
+
+def create_payment_attempt(
+    db: Session,
+    order_id: int,
+    final_amount: float 
+) -> PaymentAttempt:
+    
+    payment_attempt = PaymentAttempt(
+        order_id=order_id,
+        status=PaymentStatus.PENDING,
+        amount_attempted=final_amount,
+        final_amount=final_amount
+    )
+
+    payment_attempt = repo.create_payment_attempt(db, payment_attempt)
+
+    event = payment_attempt_pending_event(
+        payment_attempt_id=payment_attempt.id,
+        order_id=order_id,
+        amount_attempted=final_amount,
+        final_amount=final_amount
+    )
+    publish_event(event["type"], event["payload"])
+
+    return payment_attempt
+
+
+
+def mark_payment_succeeded(
+    db: Session,
+    payment_attempt_id: int,
+    provider_payment_id: str 
+):
+    payment_attempt = repo.get_payment_attempt_by_id(db, payment_attempt_id)
+    if not payment_attempt:
+        raise NotFoundError("Payment attempt not found")
+    
+    if payment_attempt.status == PaymentStatus.SUCCEEDED:
+        raise ConflictError("Payment attempt already succeeded")
+    
+    if payment_attempt.status == PaymentStatus.FAILED:
+        raise ConflictError("Payment attempt has failed")
+    
+    if payment_attempt.status != PaymentStatus.PENDING:
+        raise ConflictError("Payment attempt not pending")
+
+    order = payment_attempt.order
+    
+    if payment_attempt.final_amount != order.final_amount:
+        raise ValidationError("Final amount not equal to order amount")
+    
+    payment_attempt.status = PaymentStatus.SUCCEEDED
+
+    payment_attempt.provider_payment_id = provider_payment_id
+
+    repo.save(db, payment_attempt)
+
+    event = payment_attempt_succeeded_event(
+        payment_attempt_id=payment_attempt_id,
+        order_id=order.id,
+        final_amount=payment_attempt.final_amount
+    )
+    publish_event(event["type"], event["payload"])
+
+    return payment_attempt
+
+
+
+def mark_payment_failed(
+    db: Session,
+    payment_attempt_id: int,
+    failure_reason: str
+):
+    payment_attempt = repo.get_payment_attempt_by_id(db, payment_attempt_id)
+    if not payment_attempt:
+        raise NotFoundError("Payment attempt not found")
+    
+    if payment_attempt.status == PaymentStatus.SUCCEEDED:
+        raise ConflictError("Payment attempt already succeeded")
+    
+    if payment_attempt.status == PaymentStatus.FAILED:
+        raise ConflictError("Payment attempt already Failed")
+    
+    if payment_attempt.status != PaymentStatus.PENDING:
+        raise ConflictError("Payment attempt not pending")
+    
+    payment_attempt.status = PaymentStatus.FAILED
+
+    payment_attempt.failure_reason = failure_reason
+
+    repo.save(db, payment_attempt)
+
+    event = payment_attempt_failed_event(
+        payment_attempt_id=payment_attempt_id,
+        failure_reason=payment_attempt.failure_reason,
+        order_id=payment_attempt.order_id
+    )
+    publish_event(event["type"], event["payload"])
+
+    return payment_attempt
+
